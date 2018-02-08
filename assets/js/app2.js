1dcd094cad8e0c27615c79cf536eca2ffda3aa8e
Claudina Cortez de Leon <claudinacr@gmail.com>
2018-02-08 11:28:01 -0300

    Mejora

--- assets/js/app2.js
+++ assets/js/app2.js
@@ -12,7 +12,14 @@ function drop(ev) {
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
     var idFirst = document.querySelector('#idFirst').childElementCount;
+    var idFirst2 = document.querySelector('#idFirst').childNodes;
+
     console.log(idFirst);
+
+    if (idFirst < 3) {
+        alert('Faltan ' + (3 - idFirst) + ' imagenes para completar el collage');
+    }
+    if (idFirst === 3) {
+        alert('Tiene la cantidad mínima de imagenes en su collage');
+    }
 }
-
-
